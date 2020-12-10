03ca3b7a276af98ef588d784e69d0197c944265e
Leonardo Capogna <leonardocapogna@gmail.com>
2020-12-10 12:24:07 +0100

    start repo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Esercizio:
+// - creare una lista di cose da fare e mostrarla in pagina con Vue;
+// - aggiungere un campo di input e un pulsante
+//  per aggiungere nuove cose da fare alla lista;
+// - cancellare un item dalla lista, al click sulla relativa icona 'X'.
+// Bonus:
+// - Oltre al click sul pulsante,
+// consentire l'inserimento del nuovo item anche premendo
+//  il tasto Invio della tastiera,
+//  dopo aver scritto il testo nel campo di input.
+// Consiglio del giorno: per alcuni task, potrebbe essere necessario fare
+// delle ricerche e capire come lavorano alcune funzioni o direttive
+//  leggendo le documentazioni ufficiali ed analizzando i relativi esempi.
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        list: [
+            "crea la cartella",
+            "nomina la cartella",
+            "inserisci sottocartelle js e css",
+            "controlla le slide"
+            "inizia a sclerare"
+        ],
+      },
+});
